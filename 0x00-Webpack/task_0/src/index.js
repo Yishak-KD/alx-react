52fef1a47d0d9dfbb2308c2445b9213cfe66f67e
Yishak Kidanu <yisak04@gmail.com>
2023-01-23 13:21:08 +0000

    Configuring Webpack

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+console.log("Holberton Dashboard")
+console.log("\n")
+console.log("Dashboard data for the students")
+console.log("\n")
+console.log("Copyright - Holberton School")

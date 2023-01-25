d1b1177e80691dec8adb62d7d9884ea571b3a7fe
Yishak Kidanu <yisak04@gmail.com>
2023-01-25 12:17:23 +0300

    Creating a basic react app

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import holberton from "./Holberton.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holberton} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <hr />
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <hr />
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;

52a99f629aa0ad0263dd0bcb939dce1a62158e39
Yishak Kidanu <yisak04@gmail.com>
2023-02-03 11:29:47 +0300

    Convert Functional Component to Class component for App.js

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import "./App.css";
+import Notifications from '../Notifications/Notifications'
+import Login from '../Login/Login'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+import NotificationItem from '../Notifications/NotificationItem';
+// import CourseListRow from '../CourseList/CourseListRow';
+import CourseList from '../CourseList/CourseList';
+import propTypes from 'prop-types';
+
+class App extends React.Component {
+  render() {
+    if (this.props.isLoggedIn === true) {
+      return (
+        <>
+          <Notifications displayDrawer={true} />
+          <div className="App">
+            <Header />
+            <NotificationItem />
+            <CourseList />
+            <Footer />
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Notifications />
+          <div className="App">
+            <Header />
+            <NotificationItem />
+            <Login />
+            <Footer />
+          </div>
+        </>
+      )
+    }
+  }
+}
+// function App({ isLoggedIn }) {
+//   
+//     return (
+
+//     )
+//   }
+// }
+
+App.propTypes = {
+  isLoggedIn: propTypes.bool
+}
+
+App.defaultProps = {
+  isLoggedIn: false
+}
+
+export default App;

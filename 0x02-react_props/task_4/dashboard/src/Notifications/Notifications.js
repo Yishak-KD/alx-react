81531a8cca16cf2f475570f8c559fa3e03b6de63
Yishak Kidanu <yisak04@gmail.com>
2023-02-07 01:02:46 +0300

    Notifications component modified

--- 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
@@ -7,21 +7,24 @@ import propTypes from 'prop-types';
 
 function notif({ displayDrawer }) {
     if (displayDrawer === true) {
-        return (<>
-            <div className='menuItem'>Your notifications</div>
-
-            <div className='Notifications'>
-                <button style={{ float: 'right' }} aria-label="close" onClick={() => console.log("Close button has been clicked")}>
-                    <img src={image} alt="" />
-                </button>
-                <p>Here is the list of notifications</p>
-                <ul>
-                    <NotificationItem type="default" value="New course available" />
-                    <NotificationItem type="urgent" value="New resume available" />
-                    <NotificationItem type="urgent" html={getLatestNotification()} />
-                </ul>
-            </div>
-        </>)
+        return (
+            <>
+                <div className='menuItem'>Your notifications</div>
+                <div className='Notifications'>
+                    <button style={{ float: 'right' }} aria-label="close" onClick={() => console.log("Close button has been clicked")}>
+                        <img src={image} alt="" />
+                    </button>
+                    <p>Here is the list of notifications</p>
+                    <ul>
+                        <NotificationItem type="default" value="New course available" />
+                        <NotificationItem type="urgent" value="New resume available" />
+                        <NotificationItem type="urgent" html={getLatestNotification()} />
+                    </ul>
+                </div>
+            </>
+        )
+    } else {
+        return (<div className='menuItem'>Your notifications</div>)
     }
 }
 

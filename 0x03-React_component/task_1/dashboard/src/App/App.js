7ec7e275d0072bdf9919bc3b69e83bfa7ef44440
Yishak Kidanu <yisak04@gmail.com>
2023-02-06 14:19:37 +0300

    Add eventlistener

---
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import "./App.css";
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import NotificationItem from '../Notifications/NotificationItem';
+import CourseList from '../CourseList/CourseList';
+import propTypes from 'prop-types';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'h' && e.ctrlKey) {
+      alert('Logging you out!')
+      this.logOut();
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress)
+  }
+
+  render() {
+    if (this.props.isLoggedIn === true) {
+      return (
+        <>
+          <Notifications displayDrawer={true} />
+          <div className="App">
+            <Header />
+            <NotificationItem />
+            <CourseList />
+            <Footer />
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Notifications />
+          <div className="App">
+            <Header />
+            <NotificationItem />
+            <Login />
+            <Footer />
+          </div>
+        </>
+      )
+    }
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: propTypes.bool,
+  logOut: propTypes.func
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => null
+}
+
+export default App;

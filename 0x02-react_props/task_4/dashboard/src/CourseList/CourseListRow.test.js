173de127aaed5f1d325d519c2fa6e20bb49589d0
Yishak Kidanu <yisak04@gmail.com>
2023-02-11 03:12:33 +0300

    Test added for Course List

--- 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import CourseListRow from "./CourseListRow";
 import { shallow } from 'enzyme';
-
+//
 describe("Test the Table file", () => {
     test("Component renders one cell", () => {
         const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell={null} />)
 
-        expect(wrapper.find('tr').children()).toHaveLength(1)
+        expect(wrapper.find('th').length).toBe(1)
     })
     test("Component renders two cell when rendered", () => {
-        const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell={test} />)
+        const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell="test" />)
 
-        expect(wrapper.find('tr').children()).toHaveLength(2)
+        expect(wrapper.find('th').length).toBe(2)
     })
     test("Component renders two td", () => {
-        const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell={null} />)
+        const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="null" />)
 
-        expect(wrapper.find('tr').children()).toHaveLength(2)
+        expect(wrapper.find('tr td').length).toBe(2)
     })
 })

2f959ec4776b504f9ed73ff78b6810eb231839f8
Yishak Kidanu <yisak04@gmail.com>
2023-02-11 04:14:27 +0300

    Test added for the App component

--- 0x02-react_props/task_4/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.test.js
@@ -5,6 +5,7 @@ import Notifications from '../Notifications/Notifications'
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
 
 describe("Test App component", () => {
     test("Renders without crushing", () => {
@@ -28,4 +29,17 @@ describe("Test App component", () => {
         const component = shallow(<App />)
         expect(component.contains(<Footer />)).toBe(true)
     })
+
+    test("Check that CourseList is not displayed", () => {
+        const component = shallow(<App isLoggedIn={false} />)
+        expect(component.contains(<CourseList />)).toBe(false)
+    })
+
+    test("Check that CourseList is displayed, and login component is not included", () => {
+        const component = shallow(<App isLoggedIn={true} />)
+
+        expect(component.contains(<CourseList />)).toBe(true)
+        expect(component.contains(<Login />)).toBe(false)
+    })
+
 })

e23168e5f38c53bc80cc3b251e838eee74d719a6
Yishak Kidanu <yisak04@gmail.com>
2023-02-13 14:17:25 +0300

    Convert functional component to React class

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,55 +1,42 @@
 import React from 'react';
 import "./App.css";
-import Notifications from '../Notifications/Notifications';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
+import Notifications from '../Notifications/Notifications'
+import Login from '../Login/Login'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
 import NotificationItem from '../Notifications/NotificationItem';
+// import CourseListRow from '../CourseList/CourseListRow';
 import CourseList from '../CourseList/CourseList';
 import propTypes from 'prop-types';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 }
+  ];
 
   render() {
-    if (this.props.isLoggedIn === true) {
-      return (
-        <>
-          <Notifications displayDrawer={true} />
-          <div className="App">
-            <Header />
-            <NotificationItem />
-            <CourseList />
-            <Footer />
-          </div>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <Notifications />
-          <div className="App">
-            <Header />
-            <NotificationItem />
-            <Login />
-            <Footer />
-          </div>
-        </>
-      )
-    }
+    return (
+      <>
+        <Notifications displayDrawer={true} />
+        <div className="App">
+          <Header />
+          <NotificationItem />
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </>
+    )
   }
 }
 
 App.propTypes = {
-  isLoggedIn: propTypes.bool,
-  logOut: propTypes.func
+  isLoggedIn: propTypes.bool
 }
 
 App.defaultProps = {
-  isLoggedIn: false,
-  logOut: () => null
+  isLoggedIn: false
 }
 
 export default App;

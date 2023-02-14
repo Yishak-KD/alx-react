55a91831aeb09390adea4fda212f830c849a979f
Yishak Kidanu <yisak04@gmail.com>
2023-02-14 15:27:12 +0300

    New component added

---
+++ 0x03-React_component/task_3/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,49 @@
+import propTypes from 'prop-types';
+import React from 'react'
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  if (isHeader === true) {
+    if (textSecondCell === null) {
+      return (
+        <>
+          <tr>
+            <th colSpan={2}>{textFirstCell}</th>
+          </tr>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <tr>
+            <th>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
+          </tr>
+        </>
+      )
+    }
+  } else {
+    return (
+      <>
+        <tr>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
+        </tr>
+      </>
+    )
+  }
+}
+
+CourseListRow.propTypes = {
+  isHeader: propTypes.bool,
+  textFirstCell: propTypes.string.isRequired,
+  textSecondCell: propTypes.oneOfType([
+    propTypes.string.isRequired,
+    propTypes.number.isRequired])
+}
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null
+}
+
+export default CourseListRow;
---
+++ 0x03-React_component/task_3/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import "./Notifications.css";
+import image from '../close-icon.png';
+import NotificationItem from './NotificationItem';
+import propTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
+
+class notif extends React.Component {
+
+    markAsRead = id => {
+        if (typeof id === 'number')
+            console.log(`Notification ${id} has been marked as read`)
+    }
+
+    render() {
+        if (this.props.displayDrawer === true) {
+            return (
+                <>
+                    <div className='menuItem'>Your notifications</div>
+                    <div className='Notifications'>
+                        <button style={{ float: 'right' }} aria-label="close" onClick={() => console.log("Close button has been clicked")}>
+                            <img src={image} alt="" />
+                        </button>
+                        <p>Here is the list of notifications</p>
+                        <ul>
+                            {
+                                this.props.listNotifications.length === 0 ? <NotificationItem type="default" value="No new notification for now" /> : null
+                            }
+                            {
+                                this.props.listNotifications.map((val, index) => {
+                                    return <NotificationItem type={val.type} value={val.value} html={val.html} key={val.index} />
+                                })
+                            }
+                        </ul>
+                    </div>
+                </>
+            )
+        } else {
+            return (<div className='menuItem'>Your notifications</div>)
+        }
+    }
+}
+
+notif.propTypes = {
+    displayDrawer: propTypes.bool,
+    listNotifications: propTypes.arrayOf(NotificationItemShape)
+}
+
+notif.defaultProps = {
+    displayDrawer: false,
+    listNotifications: []
+}
+
+export default notif;

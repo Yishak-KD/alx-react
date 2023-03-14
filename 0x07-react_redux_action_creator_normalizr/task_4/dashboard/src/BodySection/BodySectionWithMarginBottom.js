45e5b0dbf5b030d4969bb2964484bffdc6faba23
Yishak Kidanu <yisak04@gmail.com>
2023-03-14 11:33:51 +0300

    Creating an action for the UI

---
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import BodySection from './BodySection';
+import { StyleSheet, css } from 'aphrodite'
+
+class BodySectionWithMarginBottom extends Component {
+    render() {
+        return (
+            <div className={css(styles.bodySectionWithMargin)}>
+                <BodySection {...this.props} />
+            </div>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    bodySectionWithMargin: {
+        marginBottom: '40px',
+    }
+})
+
+export default BodySectionWithMarginBottom
